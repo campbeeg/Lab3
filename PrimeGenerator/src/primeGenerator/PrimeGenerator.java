6d2aa25176e42a036d53d84345e14a8062fc57b7
Ethan Campbell <campbeeg@rose-hulman.edu>
2015-03-27 17:21:26 -0400

    Test 8 written and passes

--- PrimeGenerator/src/primeGenerator/PrimeGenerator.java
+++ PrimeGenerator/src/primeGenerator/PrimeGenerator.java
@@ -7,14 +7,22 @@ public class PrimeGenerator {
 
 	public List<Integer> generate(final Integer i) {
 		ArrayList<Integer> primes = new ArrayList<Integer>();
-		if (i > 2)
+
+		if (i > 2) {
 			primes.add(2);
-		if (i > 3)
-			primes.add(3);
-		if (i > 5)
-			primes.add(5);
-		
+
+			for (int j = 2; j < i; j++) {
+				for (int k = 2; k < j; k++) {
+					if (j % k == 0) {
+						break;
+					}
+					if (k == (j - 1))
+						primes.add(j);
+				}
+			}
+		}
+
 		return primes;
 	}
-	
+
 }
--- PrimeGenerator/src/primeGenerator/PrimeGeneratorTest.java
+++ PrimeGenerator/src/primeGenerator/PrimeGeneratorTest.java
@@ -38,7 +38,8 @@ public class PrimeGeneratorTest {
          { 2, list() },
          { 3, list(2) },
          { 4, list(2, 3) },
-         { 6, list(2, 3, 5) }
+         { 6, list(2, 3, 5) },
+         { 8, list(2, 3, 5, 7) }
       });
    }
 
